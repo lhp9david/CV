11cf7c8995b2132ab561909d047dfaf86bb7c221
David Godard <davidgodard76700@gmail.com>
2023-08-15 17:40:12 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let icone = document.querySelectorAll('.info-icone');
+let competence = document.querySelector('.competence-saverglass');
+
+
+icone.forEach(element => {
+    element.addEventListener('click', () => {
+        const ulElement = element.closest('.poste-date').nextElementSibling;
+        ulElement.classList.toggle('active');
+    })
+    
+});
